0e8eb25e96b030922b4a27dcababdabbcbc85510
MarengoSimone <s.marengo.1338@vallauri.edu>
2022-01-20 10:42:08 +0100

    Started router

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+
+const appRoutes : Routes = [
+  {
+    path:'',
+    redirectTo:'/recipes',
+    pathMatch:'full' // solo se c'è '' e non su tutte le routes
+  },
+  {
+    path:'recipes',
+    component:RecipeListComponent
+  },
+  {
+    path:'shopping-list',
+    component:ShoppingListComponent
+  }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    [RouterModule.forRoot(appRoutes)]
+  ],
+  exports:[RouterModule]
+})
+export class AppRoutingModule {
+  
+}
